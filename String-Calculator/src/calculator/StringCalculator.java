47c015db4d88a2058b6303afb8e65a430531ed57
RenukaJoshi <joshirenukab@gmail.com>
2021-10-16 21:15:53 +0530

    passed test string_support_different_delimiter

--- String-Calculator/src/calculator/StringCalculator.java
+++ String-Calculator/src/calculator/StringCalculator.java
@@ -1,8 +1,12 @@
 package calculator;
 
-class StringCalculator {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+class StringCalculator {
+	
     public int add(String input) {
+
     	String[] numbers = input.split(",|\n");
     	if(input.isEmpty()) {
     		return 0;
@@ -12,12 +16,18 @@ class StringCalculator {
     		return Integer.parseInt(input);
     		
     	}
+    	else if(input.startsWith("//"))
+    	{
+    		String[] del =  getDelimiter(input);
+    		return getSum(del);
+    	}
     	else {
     		return getSum(numbers);
     	}
     
     }
     
+    //Method to handle an unknown amount of numbers
     private int getSum(String[] numbers)
     {
     	int sum = 0;
@@ -28,5 +38,20 @@ class StringCalculator {
 		return sum;
     	
     }
+    
+    //Method to support different delimiters
+    private static String[] getDelimiter(String input)
+    {
+    		Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+    		if(matcher.matches())
+    		{
+    			String delimiter = matcher.group(1);
+    			String toSplit = matcher.group(2);
+    			return toSplit.split(delimiter);
+    		}
+    		throw new RuntimeException("Wrong custom delimiter format");
+    }
+   
+    
 
 }
